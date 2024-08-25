43635b2363d26a2d3d0af3e8871f6a705380f48c
RBrunning <you@example.com>
2024-08-25 18:53:04 +0100

    part1: 1.6

--- submissions/part1/unicafe/src/App.jsx
+++ submissions/part1/unicafe/src/App.jsx
@@ -1,34 +1,46 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const GOOD = "good";
+const NEUTRAL = "neutral";
+const BAD = "bad";
+
+const Button = ({type, onClick}) => {
+  return (
+    <button onClick={onClick}>{type}</button>
+  )
+}
+
+const Result = ({type, result}) => {
+  return (
+    <div>
+      {type}{"  "}{result}
+    </div>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => setGood(good + 1)
+  const handleNeutralClick = () => setNeutral(neutral + 1)
+  const handleBadClick = () => setBad(bad + 1)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h2>give feedback</h2>
+
+      <Button type={GOOD} onClick={handleGoodClick} />
+      <Button type={NEUTRAL} onClick={handleNeutralClick} />
+      <Button type={BAD} onClick={handleBadClick} />
+
+      <h2>statistics</h2>
+      <Result type={GOOD} result={good} />
+      <Result type={NEUTRAL} result={neutral} />
+      <Result type={BAD} result={bad} />
+
+    </div>
   )
 }
 
