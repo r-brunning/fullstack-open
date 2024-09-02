2d3b2191955498c753ac99469825e2437ba0ff3f
RBrunning <you@example.com>
2024-09-02 16:49:00 +0100

    part2: 2.11 and part 2 notes

--- submissions/part2/phonebook/src/App.jsx
+++ submissions/part2/phonebook/src/App.jsx
@@ -1,18 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Filter } from "./components/Filter";
 import { Numbers } from "./components/Numbers";
 import { PersonForm } from "./components/PersonForm";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
+
+  const hook = () => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  };
+
+  useEffect(hook, []);
 
   const handleSearchChange = (event) => {
     const newSearchTerm = event.target.value;
