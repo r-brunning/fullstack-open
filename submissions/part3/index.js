1700545f7af32b84d46244c1dfaaea591caf6c99
RBrunning <you@example.com>
2024-10-03 15:06:39 +0100

    3.2: phonebook - Add /info route to display phonebook entry count and request timestamp

--- submissions/part3/index.js
+++ submissions/part3/index.js
@@ -28,6 +28,14 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+  const currentDate = new Date(); 
+  res.send(`
+    <p>Phonebook has info for ${persons.length} people.</p>
+    <p>${currentDate}</p>
+  `)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
